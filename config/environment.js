45eef53bd6966d9c88cddb7b82f385b6fe9a9b52
Francisco Dias <francisco.ffbhd@gmail.com>
2019-08-21 10:10:07 +0100

    francisco update wednesday am

--- config/environment.js
+++ config/environment.js
@@ -2,6 +2,7 @@ const port = process.env.PORT || 4000
 const env = process.env.NODE_ENV || 'development'
 const dbURI = process.env.MONGODB_URI || `mongodb://localhost:27017/project3-datingExp-db-${env}`
 const secret = process.env.SECRET || 'Tgs5aG_^GH@lKmnN=++/dgyhhebded'
+const fileloaderKey = 'AYGiGjrSKpaWKAEudgKALz'
 
 
 const USER = 'daframiso.project3@gmail.com'
@@ -9,4 +10,4 @@ const PASS = 'daframiso2019'
 
 
 
-module.exports = { port, env, dbURI, secret, USER, PASS }
+module.exports = { port, env, dbURI, secret, USER, PASS, fileloaderKey }
